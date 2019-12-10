9b9c9c1938204d01d9a6b0004b4720afe3b26562
KARTIK <kartikkapoor475@gmail.com>
2019-12-10 13:57:07 +0530

    Updates

---
+++ precache-manifest.c81aa27455061afc55a9f50e25bfd3e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8dda86e5a34e4fdbbaf5b222c41d2c4",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "c5fd24cd2d17bd1aec2d",
+    "url": "/phone-directory/static/css/main.f36a1571.chunk.css"
+  },
+  {
+    "revision": "65349452b1b202790f4a",
+    "url": "/phone-directory/static/js/2.aac8df68.chunk.js"
+  },
+  {
+    "revision": "c5fd24cd2d17bd1aec2d",
+    "url": "/phone-directory/static/js/main.b20d9c74.chunk.js"
+  },
+  {
+    "revision": "6bbcdb61204eec454ffa",
+    "url": "/phone-directory/static/js/runtime-main.a5ae7367.js"
+  }
+]);
